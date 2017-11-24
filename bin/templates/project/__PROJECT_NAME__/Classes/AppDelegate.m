e661503c3e932f9d7aff52ab9455daf34c659e3c
Karol Szafrański <kszafranski@eclipsesource.com>
2017-11-24 17:27:55 +0100

    Bridge Tabris with cordova
    main.m
    - Use AppDelegate based on TabrisClientAppDelegate
    - Use TabrisClientApplication

--- bin/templates/project/__PROJECT_NAME__/Classes/AppDelegate.m
+++ bin/templates/project/__PROJECT_NAME__/Classes/AppDelegate.m
@@ -26,14 +26,46 @@
 //
 
 #import "AppDelegate.h"
-#import "MainViewController.h"
+#import <Cordova/CDVPlugin.h>
+#import "CordovaPluginBridge.h"
+#import "CordovaConfig.h"
+/* HOOK: import classes for registration */
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
-{
-    self.viewController = [[MainViewController alloc] init];
-    return [super application:application didFinishLaunchingWithOptions:launchOptions];
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [super application:application didFinishLaunchingWithOptions:launchOptions];
+    /* HOOK: applicationDidFinishLaunching */
+    return YES;
+}
+
+- (NSURL *)packageJsonUrl {
+    return [[[[NSBundle mainBundle] resourceURL] URLByAppendingPathComponent:@"www" isDirectory:YES] URLByAppendingPathComponent:[CordovaConfig config].packageJsonPath];
+}
+
+- (BOOL)useSSLStrict {
+    id sslSetting = [[CordovaConfig config].settings objectForKey:@"usestrictssl"];
+    if( sslSetting ) {
+        return [sslSetting boolValue];
+    }
+    return YES;
+}
+
+- (BOOL)prefersStatusBarHidden {
+    id fullscreenSetting = [[CordovaConfig config].settings objectForKey:@"fullscreen"];
+    if( fullscreenSetting ) {
+        return [fullscreenSetting boolValue];
+    }
+    return NO;
+}
+
+- (BOOL)enableDeveloperConsole {
+    return [[[CordovaConfig config].settings objectForKey:@"enabledeveloperconsole"] boolValue];
+}
+
+- (void)tabrisClientWillStartExecuting:(TabrisClient *)tabrisClient {
+    [tabrisClient addRemoteObject:[CordovaPluginBridge class]];
+    /* HOOK: tabrisClientWillStartExecuting */
 }
 
 @end
